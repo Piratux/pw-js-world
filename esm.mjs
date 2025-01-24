d8901538dc90b7140daa868a14a6a5195f684a06
Piratux <58703216+Piratux@users.noreply.github.com>
2025-01-24 22:49:01 +0200

    test

--- esm.mjs
+++ esm.mjs
@@ -1,29 +1,13 @@
-import {ComponentTypeHeader, default as BufferReader} from "./dist/BufferReader.js";
-
 // TODO: clean this code to make it easier to get named exports.
-let PWGameWorldHelper = (await import("./dist/Helper.js")).default;
-let Block = (await import("./dist/Block.js")).default;
 
-let Player = (await import("./dist/Player.js"));
-
-let PlayerEffect = Player.PlayerEffect;
-
-Player = Player.default;
-
-if ("default" in PWGameWorldHelper) PWGameWorldHelper = PWGameWorldHelper.default;
-if ("default" in Block) Block = Block.default;
-if ("default" in Player) Player = Player.default;
-
-const Constants = (await import("./dist/Constants.js")).default;
+import PWGameWorldHelper from "./dist/Helper.js";
+import Block from "./dist/Block.js";
+import BufferReader, { ComponentTypeHeader } from "./dist/BufferReader.js";
+import Player, { PlayerEffect } from "./dist/Player.js";
+import { LayerType } from "./dist/Constants.js";
 
 export default {
     PWGameWorldHelper, Block, BufferReader,
-    Player, Constants, ComponentTypeHeader,
+    Player, LayerType, ComponentTypeHeader,
     PlayerEffect
-};
-
-export {
-    PWGameWorldHelper, Block, BufferReader,
-    Player, Constants, ComponentTypeHeader,
-    PlayerEffect
-};
+}
